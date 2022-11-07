97e9688878f0428af4cef59e4bdb89151b9d34e1
CodeMaker-Zhao <zlq1440169549@qq.com>
2022-11-07 15:34:11 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app" style="display: flex; position: relative">
+    <img src="./assets/bg.jpg" style="width: 100%" />
+    <img v-if="img_url" :src="img_url" class="pic" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+function getImg(that) {
+  axios
+    .get("http://localhost:4000", { responseType: "arraybuffer" })
+    .then(response => {
+      let blob = new Blob([response.data], {
+        type: response.headers["content-type"]
+      });
+      let image = URL.createObjectURL(blob);
+      that.img_url = image;
+    });
+}
+
+export default {
+  name: "App",
+  created() {
+    const that = this;
+    getImg(that);
+    setInterval(() => {
+      getImg(that);
+    }, 3000);
+  },
+  data() {
+    return {
+      img_url: ""
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.pic {
+  position: absolute;
+  width: 100%;
+}
+</style>
